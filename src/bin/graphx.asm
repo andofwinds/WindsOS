e84d5a5647941f636b89c85af4ecbe02bef760ea
andofwinds <ba289mail@mail.ru>
2024-03-17 02:53:53 +0300

    Just some updates for GraphX

--- src/bin/graphx.asm
+++ src/bin/graphx.asm
@@ -15,13 +15,59 @@ start:
     call puts
 
     mov ah, 0x0c
-    mov al, 0xFF
-    mov dx, 100
-    mov cx, 100
+    mov al, 0x7
+    mov dx, 20
+    mov cx, 20
     xor bh, bh
+
+    call init_workspace
+
+    mov cx, 320
+    mov dx, 175
     int 0x10
 
+    call draw_cursor
+
     jmp mainloop
+
+init_workspace:
+
+    jmp .yfill_loop
+
+.yfill_loop:
+    cmp dx, 340
+    je .after_y
+    inc dx
+    int 0x10
+
+    mov cx, 20
+    int 0x10
+
+    mov cx, 620
+    int 0x10
+
+    jmp .yfill_loop
+
+.after_y:
+    mov dx, 20
+    mov cx, 20
+
+.xfill_loop:
+    cmp cx, 620
+    je .after_x
+    inc cx
+    int 0x10
+
+    mov dx, 20
+    int 0x10
+    mov dx, 340
+    int 0x10
+
+    jmp .xfill_loop
+
+
+.after_x:
+    ret
 
 
 mainloop:
@@ -72,14 +118,66 @@ write:
     jmp mainloop
 
 
+draw_cursor:
+    mov ah, 0x0c
+    mov al, 0xFF
+    xor bh, bh
+    xor si, si
 
+    add dx, 3
+    sub cx, 2
+
+.draw_x:
+    cmp si, 5
+    je .after_x
+    int 0x10
+
+    add dx, 4
+    int 0x10
+
+    sub dx, 4
+
+    inc cx
+    inc si
+
+    jmp .draw_x
+
+.after_x:
+
+    xor si, si
+    sub cx, 5
+
+    jmp .draw_y
+
+.draw_y:
+    cmp si, 5
+    je .after_y
+    int 0x10
+
+    add cx, 4
+    int 0x10
+
+    sub cx, 4
+
+    inc dx
+    inc si
+
+    jmp .draw_y
+
+.after_y:
+    sub dx, 3
+    add cx, 2
+
+    int 0x10
+
+    ret
 
 %include "src/tools/inpututils.asm"
     input_proc:
             jmp start
     input_buffer: times 64 db 0
 
-msg_hello:              db 'Welcome to Forsaken GraphX', ENDL, 0
+msg_hello:              db 'Forsaken GraphX', ENDL, 0
 NEWLINE:                db ENDL, 0
 msg_read_failed:        db 'Read from disk failed!', ENDL, 0
 msg_kernel_not_found:   db 'file not found!', ENDL, 0
